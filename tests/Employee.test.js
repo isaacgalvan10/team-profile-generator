6edb1ec4ef98d5271824632ca4e235523942bdf4
Isaac Galvan <isaacgdev@gmail.com>
2022-01-21 22:21:27 -0600

    initializing project files

---
+++ tests/Employee.test.js
@@ -0,0 +1,7 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+  describe('initialization', () => {
+    it('should ')
+  })
+})
---
+++ tests/Manager.test.js
@@ -0,0 +1,7 @@
+// const Manager = require('../lib/Manager');
+
+// describe('Manager', () => {
+//   it('it should ', () => {
+//     expect(new Manager('Isaac'))
+//   })
+// })
